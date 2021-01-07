fdecc28b7fd99b6c144c7280ad363d6dd2a80bd3
codervibe <3507557934@qq.com>
2021-01-07 14:05:15 +0800

    修复了输出文件会乱码的BUG

--- src/com/codervibe/EncryptionAndDecryptionFile.java
+++ src/com/codervibe/EncryptionAndDecryptionFile.java
@@ -4,13 +4,11 @@ import com.Untils.Base64s;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.PrintWriter;
+import java.io.*;
 
 /**
  * Created by Administrator on 2020/12/7  0007
+ *
  * @author Administrator
  * DateTime:2020/12/07 21:21
  * Description: 加密文件和解密文件的windows 图形界面 1.0.0
@@ -30,7 +28,7 @@ public class EncryptionAndDecryptionFile extends JFrame {
     /**
      * 文件按钮
      */
-    JButton button1 = new JButton("文件");
+    JButton button1 = new JButton("...");
     /**
      * 加密按钮
      */
@@ -63,9 +61,9 @@ public class EncryptionAndDecryptionFile extends JFrame {
         //窗体最上方的标题类似于<title></title>
         setTitle("加密与解密");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
         //文本域的大小和位置
         textfield.setBounds(100, 89, 400, 30);
         //将这个文本域添加到窗体上
@@ -111,30 +109,34 @@ public class EncryptionAndDecryptionFile extends JFrame {
 
                 //根据数据源创建字符输入流对象
                 //IO流读入要加密的文件
-                BufferedReader br = new BufferedReader(new FileReader(textfield.getText()));
+                BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(textfield.getText()), "UTF-8"));
+
+
                 String pwPath = getpath(path, name);//将路径中的文件名去掉留下纯文件名
                 String pwPathFileName = pwPath + "加密后的" + name;
                 //根据目的地创建字符输出流对象
                 // IO流将加密后的文件写出  pwPath + "加密后的" + Name 写出的位置 和文件名
-                PrintWriter pw = new PrintWriter(new FileWriter(pwPathFileName), true);
-
+                BufferedWriter pw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(pwPathFileName), "UTF-8"));
+                StringBuffer sbr = new StringBuffer();
+                String s = null;
 
                 //读写数据，复制文件
-                String line;
-                while ((line = br.readLine()) != null) {
-                    //这个if 区间就是加密的部分了
-                    byte[] str = line.getBytes();
+                while ((s = br.readLine()) != null) {
+
+
+                    byte[] str = s.getBytes();
                     String buttonName = e.getActionCommand();
                     //按钮的名字
                     String anticipationsButtonName = "加密";
+                    //这个if 区间就是加密的部分了
                     if (buttonName.equals(anticipationsButtonName)) {
 
-                        //加密算法改为 MD5 加密
+//                        加密算法改为 MD5 加密
                         try {
                             result = Base64s.encryptBASE64(str);
-                            textfield2.setText(result);
 
-                            pw.println(result);
+
+                            sbr.append(result);
 
                         } catch (Exception exception) {
                             exception.printStackTrace();
@@ -142,51 +144,56 @@ public class EncryptionAndDecryptionFile extends JFrame {
                         }
 
                     }
-                }
 
+                }
+                s = sbr.toString();
+
+                //文件写出
+                pw.write(s);
                 //释放资源
                 pw.close();
                 br.close();
                 textfield2.setText(pwPath + "加密后的" + name);
                 System.out.println("加密完成!!!!!!");
-                //IO流读取写出时会出现异常 这里是用来捕捉的
-
             } catch (Exception exception) {
+                /*IO流读取写出时会出现异常 这里是用来捕捉的*/
                 exception.printStackTrace();
             }
-
-
         });
-        //解密按钮监听事件
         button3.addActionListener(e -> {
-            byte[] result;
-
+            /*解密按钮监听事件*/
+            byte[] result = new byte[0];
             try {
-
                 //根据数据源创建字符输入流对象
-                BufferedReader br = new BufferedReader(new FileReader(textfield.getText()));
-                //IO流读入要加密的文件
+                BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(textfield.getText()), "UTF-8"));
+
+                //IO流读入要解密的文件
                 String pwPath = getpath(path, name);//将路径中的文件名去掉留下纯文件名
 
                 //根据目的地创建字符输出流对象
-                // IO流将加密后的文件写出  pwPath + "加密后的" + Name 写出的位置 和文件名
-                PrintWriter pw = new PrintWriter(new FileWriter(pwPath + "解密后的" + name), true);
+                // IO流将加密后的文件写出  pwPath + "解密后的" + Name 写出的位置 和文件名
+                BufferedWriter pw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(pwPath + "解密后的" + name), "UTF-8"));
 
-                //读写数据，复制文件
-                String line;
-                while ((line = br.readLine()) != null) {
-                    //这个if 区间就是加密的部分了
+
+                StringBuffer sbr = new StringBuffer();
+                String s = null;
+                while ((s = br.readLine()) != null) {
 
                     String buttonName = e.getActionCommand();
                     //按钮的名字
                     String anticipationsButtonName = "解密";
+                    //这个if 区间就是加密的部分了
                     if (buttonName.equals(anticipationsButtonName)) {
 
-
+//                        加密算法改为 MD5 加密
                         try {
-                            result = Base64s.decryptBASE64(line);
+                            result = Base64s.decryptBASE64(s);
+
+                            sbr.append(result);
                             String str2 = new String(result);
-                            pw.println(str2);
+//                            System.out.println("result.toString() = " + str2);
+                            //文件循环写出
+                            pw.write(str2);
 
                         } catch (Exception exception) {
                             exception.printStackTrace();
@@ -194,7 +201,9 @@ public class EncryptionAndDecryptionFile extends JFrame {
                         }
 
                     }
+
                 }
+                //System.out.println("s = " + s);
 
                 //释放资源
                 pw.close();
